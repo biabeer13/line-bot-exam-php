6b47d14039a6e05f96454ec54b1990a8c712eaf0
biabeer13 <49722912+biabeer13@users.noreply.github.com>
2019-04-17 21:22:01 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,62 +1,74 @@
-<?php
-/**
- * Eric Draken
- * Date: 2016-09-02
- * Time: 4:44 PM
- * Desc: Callback for responding to Line messages
- *       Send 'whoami' to this endpoint to get a reply with your mid.
- */
- 
-// I put constants like 'LINE_CHANNEL_ID' here 
-//require_once __DIR__ . '/../includes/config.php';
-require_once "vendor/autoload.php";
-require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
- 
-use LINE\LINEBot;
-use LINE\LINEBot\HTTPClient\GuzzleHTTPClient;
- 
-// Set these values
-$config = [
-    'channelId' => '1565873395',
-    'channelSecret' => 'bc0d721e1244a24924584e0d5b93fe9a',
-    'channelMid' => '7Q6OsD1su6xYTjR5Y+8tCHBdPQZ7BiOJGAusDg2vkgq0nBfexHwmlm9c5Ae+hsplgUgC90YnXorSoLMlQGkarc8Ilkv1qCdc8elweM1oabnurSMeu6PNzXnHFrRggbBlkOnNV8fB9L85ssw8bRS/CQdB04t89/1O/w1cDnyilFU=',
-];
-$sdk = new LINEBot($config, new GuzzleHTTPClient($config));
- 
-$postdata = @file_get_contents("php://input");
-$messages = $sdk->createReceivesFromJSON($postdata);
- 
-// Verify the signature
-// REF: http://line.github.io/line-bot-api-doc/en/api/callback/post.html#signature-verification
-// REF: http://stackoverflow.com/a/541450
-$sigheader = 'X-LINE-ChannelSignature';
-$signature = @$_SERVER[ 'HTTP_'.strtoupper(str_replace('-','_',$sigheader)) ];
-if($signature && $sdk->validateSignature($postdata, $signature)) {
-    // Next, extract the messages
-    if(is_array($messages)) {
-        foreach ($messages as $message) {
-            if ($message instanceof LINEBot\Receive\Message\Text) {
-                $text = $message->getText();
-                if (strtolower(trim($text)) === "whoami") {
-                    $fromMid = $message->getFromMid();
-                    $user = $sdk->getUserProfile($fromMid);
-                    $displayName = $user['contacts'][0]['displayName'];
- 
-                    $reply = "You are $displayName, and your mid is:\n\n$fromMid";
- 
-                    // Send the mid back to the sender and check if the message was delivered
-                    $result = $sdk->sendText([$fromMid], $reply);
-                    if(!$result instanceof LINE\LINEBot\Response\SucceededResponse) {
-                        error_log('LINE error: ' . json_encode($result));
-                    }
-                } else {
-                    // Process normally, or do nothing
-                }
-            } else {
-                // Process other types of LINE messages like image, video, sticker, etc.
-            }
-        }
-    } // Else, error
-} else {
-    error_log('LINE signatures didn\'t match: ' . $signature);
-}
+<?php 
+	/*Get Data From POST Http Request*/
+	$datas = file_get_contents('php://input');
+	/*Decode Json From LINE Data Body*/
+	$deCode = json_decode($datas,true);
+
+	file_put_contents('log.txt', file_get_contents('php://input') . PHP_EOL, FILE_APPEND);
+
+	$replyToken = $deCode['events'][0]['replyToken'];
+
+	$messages = [];
+	$messages['replyToken'] = $replyToken;
+	$messages['messages'][0] = getFormatTextMessage("เอ้ย ถามอะไรก็ตอบได้");
+
+	$encodeJson = json_encode($messages);
+
+	$LINEDatas['url'] = "https://api.line.me/v2/bot/message/reply";
+  	$LINEDatas['token'] = "7Q6OsD1su6xYTjR5Y+8tCHBdPQZ7BiOJGAusDg2vkgq0nBfexHwmlm9c5Ae+hsplgUgC90YnXorSoLMlQGkarc8Ilkv1qCdc8elweM1oabnurSMeu6PNzXnHFrRggbBlkOnNV8fB9L85ssw8bRS/CQdB04t89/1O/w1cDnyilFU=";
+
+  	$results = sentMessage($encodeJson,$LINEDatas);
+
+	/*Return HTTP Request 200*/
+	http_response_code(200);
+
+	function getFormatTextMessage($text)
+	{
+		$datas = [];
+		$datas['type'] = 'text';
+		$datas['text'] = $text;
+
+		return $datas;
+	}
+
+	function sentMessage($encodeJson,$datas)
+	{
+		$datasReturn = [];
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+		  CURLOPT_URL => $datas['url'],
+		  CURLOPT_RETURNTRANSFER => true,
+		  CURLOPT_ENCODING => "",
+		  CURLOPT_MAXREDIRS => 10,
+		  CURLOPT_TIMEOUT => 30,
+		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		  CURLOPT_CUSTOMREQUEST => "POST",
+		  CURLOPT_POSTFIELDS => $encodeJson,
+		  CURLOPT_HTTPHEADER => array(
+		    "authorization: Bearer ".$datas['token'],
+		    "cache-control: no-cache",
+		    "content-type: application/json; charset=UTF-8",
+		  ),
+		));
+
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+
+		curl_close($curl);
+
+		if ($err) {
+		    $datasReturn['result'] = 'E';
+		    $datasReturn['message'] = $err;
+		} else {
+		    if($response == "{}"){
+			$datasReturn['result'] = 'S';
+			$datasReturn['message'] = 'Success';
+		    }else{
+			$datasReturn['result'] = 'E';
+			$datasReturn['message'] = $response;
+		    }
+		}
+
+		return $datasReturn;
+	}
+?>
